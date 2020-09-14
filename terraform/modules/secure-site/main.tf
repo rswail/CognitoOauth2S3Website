71dbde6bfc1a57ed2bbc1df8b90c7323f7eda53e
Ron Wail <ron@wail.net>
2020-09-14 20:31:32 +1000

    Updated to AWS Codebuild image 3

--- terraform/modules/secure-site/main.tf
+++ terraform/modules/secure-site/main.tf
@@ -444,7 +444,7 @@ resource "aws_codebuild_project" "secure-site" {
 
 	environment {
 		compute_type = "BUILD_GENERAL1_SMALL"
-		image = "aws/codebuild/standard:2.0"
+		image = "aws/codebuild/standard:3.0"
 		type = "LINUX_CONTAINER"
 
 		environment_variable {
